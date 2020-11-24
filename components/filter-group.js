cd5356c619f5e075c48812b64143f11ab2a18870
Chintan Acharya <acharya.chintan96@gmail.com>
2020-11-25 02:44:58 +0530

    Extract modal into a separate component

--- components/filter-group.js
+++ components/filter-group.js
@@ -1,5 +1,6 @@
 import { useSearchParamsContext } from "../contexts/search";
 import { useState } from "react";
+import { Modal } from "./modal";
 
 export default function FilterGroup({ name, rawName, filters }) {
 
@@ -47,28 +48,23 @@ export default function FilterGroup({ name, rawName, filters }) {
 
       {
         modalShown &&
-          <div className="fixed top-0 bottom-0 right-0 left-0 bg-black bg-opacity-10 flex justify-center items-center">
-            <div className="shadow rounded-md bg-white w-3/4">
-              <div className="border-b border-gray-300 flex justify-between">
-                <h2 className="text-2xl p-4">{name}</h2>
-                <button className="p-4 mx-2 font-bold" onClick={() => setModalShown(false)}>X</button>
-              </div>
-              <div className="flex flex-wrap p-4">
-                {
-                  filters.map(({ key, doc_count }) =>
-                    <div className="p-2 w-1/4">
-                      <button className={`text-left ${selectedCategory === key ? 'text-blue-500' : ''}`} key={key} onClick={() => {
-                        setFilter(key);
-                        setModalShown(false);
-                      }}>
-                        {key}
-                        <span className="ml-2 text-sm text-gray-400">{doc_count}</span>
-                      </button>
-                    </div>)
-                }
-              </div>
+          <Modal title={name} onClose={() => setModalShown(false)}>
+            <div className="flex flex-wrap p-4">
+              {
+                filters.map(({ key, doc_count }) =>
+                  <div className="p-2 w-1/4" key={key}>
+                    <button className={`text-left ${selectedCategory === key ? 'text-blue-500' : ''}`} key={key} onClick={() => {
+                      setFilter(key);
+                      setModalShown(false);
+                    }}>
+                      {key}
+                      <span className="ml-2 text-sm text-gray-400">{doc_count}</span>
+                    </button>
+                  </div>
+                )
+              }
             </div>
-          </div>
+          </Modal>
       }
     </div>
   );
---
+++ components/modal.js
@@ -0,0 +1,13 @@
+export function Modal({ children, onClose, title }) {
+  return (
+    <div className="fixed top-0 bottom-0 right-0 left-0 bg-black bg-opacity-10 flex justify-center items-center">
+      <div className="shadow rounded-md bg-white w-3/4">
+        <div className="border-b border-gray-300 flex justify-between">
+          <h2 className="text-2xl p-4">{title}</h2>
+          <button className="p-4 mx-2 font-bold" onClick={() => onClose && onClose()}>X</button>
+        </div>
+        {children}
+      </div>
+    </div>
+  )
+}
