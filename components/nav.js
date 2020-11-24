487e7c696af4a5e1198fb8fa5cce6ebc1861ad88
Chintan Acharya <acharya.chintan96@gmail.com>
2020-11-24 20:59:34 +0530

    Use buttons in navbar for links

--- components/nav.js
+++ components/nav.js
@@ -1,14 +1,14 @@
 export default function Nav() {
   return (
-    <nav className="bg-white font-semibold text-sm">
+    <nav className="bg-white text-sm">
       <ul className="flex items-center justify-between py-2 px-4">
-        <li className="text-blue-500">HEALTH EXPLORE</li>
+        <li className="text-blue-500 font-semibold text-lg">HEALTH EXPLORE</li>
         <ul className="flex items-center justify-between space-x-6">
-          <li>PROFILE</li>
-          <li>JOBS</li>
-          <li>PROFESSIONAL NETWORK</li>
-          <li>LOUNGE</li>
-          <li>SALARY</li>
+          <li><button className="font-semibold">PROFILE</button></li>
+          <li><button className="font-semibold">JOBS</button></li>
+          <li><button className="font-semibold">PROFESSIONAL NETWORK</button></li>
+          <li><button className="font-semibold">LOUNGE</button></li>
+          <li><button className="font-semibold">SALARY</button></li>
         </ul>
         <div className="flex space-x-6 items-stretch">
           <button className="border border-blue-500 text-blue-500 rounded-md font-semibold my-2 px-3">CREATE JOB</button>
@@ -16,7 +16,7 @@ export default function Nav() {
             <button className="bg-blue-500 h-10 w-10 rounded-full text-white font-semibold">JO</button>
             <div className="bg-red-500 text-white absolute top-0 right-0 text-xs h-6 w-6 rounded-full border-2 border-white flex justify-center items-center">2</div>
           </div>
-          <div className="flex items-center">LOGOUT</div>
+          <div className="flex items-center"><button className="font-semibold">LOGOUT</button></div>
         </div>
       </ul>
     </nav>
