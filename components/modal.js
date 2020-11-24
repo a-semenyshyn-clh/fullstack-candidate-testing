406b8dc2ffcd7bf49e775ae51534d5e8e911a73a
Chintan Acharya <acharya.chintan96@gmail.com>
2020-11-25 03:44:19 +0530

    Fix pointer events on touch

--- components/modal.js
+++ components/modal.js
@@ -1,6 +1,6 @@
 export function Modal({ children, onClose, title }) {
   return (
-    <div className="fixed top-0 bottom-0 right-0 left-0 mt-0 bg-black bg-opacity-10 flex justify-center items-center pointer-events-none">
+    <div className="fixed top-0 bottom-0 right-0 left-0 mt-0 bg-black bg-opacity-10 flex justify-center items-center">
       <div className="shadow rounded-md bg-white w-3/4">
         <div className="border-b border-gray-300 flex justify-between">
           <h2 className="text-2xl p-4">{title}</h2>
