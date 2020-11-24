a30d03c380c32a876c06fa747d586edad4bf2679
Chintan Acharya <acharya.chintan96@gmail.com>
2020-11-25 00:50:33 +0530

    Show calculated total instead of hard-coded one

--- components/job-results.js
+++ components/job-results.js
@@ -20,7 +20,6 @@ export default function JobResults({ jobs }) {
   const hospitals = jobs;
 
   const { params, setParams } = useSearchParamsContext();
-
   function handleSorterClick(key) {
 
     const { sorter } = params;
@@ -45,11 +44,13 @@ export default function JobResults({ jobs }) {
 
   }
 
+  const totalJobs = hospitals.reduce((count, hospital) => count + hospital.total_jobs_in_hospital, 0);
+
   return (
     <div className="bg-white my-4 p-6">
       <div className="flex justify-between mb-8 mt-2">
         <div>
-          <span className="font-bold">7,753</span> job postings
+          <span className="font-bold">{totalJobs}</span> job postings
         </div>
         <div className="flex space-x-4">
           <div className="text-gray-500">Sort by</div>
