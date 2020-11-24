f292c82499c1f41d177eb9f36e0807cd7421ae71
Chintan Acharya <acharya.chintan96@gmail.com>
2020-11-24 21:22:51 +0530

    Show doc_count along with filters

--- components/filter-group.js
+++ components/filter-group.js
@@ -3,8 +3,8 @@ export default function FilterGroup({ name, filters }) {
     <div className="bg-white m-4 p-4 flex flex-col space-y-2">
       <div className="font-bold mb-2">{name}</div>
       {
-        filters.map(({ key }) =>
-          <button className="text-left" key={key}>{key}</button>)
+        filters.map(({ key, doc_count }) =>
+          <button className="text-left" key={key}>{key}<span className="ml-2 text-sm text-gray-400">{doc_count}</span></button>)
       }
     </div>
   );
