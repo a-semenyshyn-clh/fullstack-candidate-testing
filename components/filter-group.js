2b205a3108dfb10a143eaeb5fb91b76ee96a0908
Chintan Acharya <acharya.chintan96@gmail.com>
2020-11-24 20:54:43 +0530

    Fix filter-group formatting

--- components/filter-group.js
+++ components/filter-group.js
@@ -1,9 +1,11 @@
 export default function FilterGroup({ name, filters }) {
-  return (<div className="bg-white m-4 p-4 flex flex-col space-y-2">
-    <div className="font-bold mb-2">{name}</div>
-    {
-      filters.map(({ key, name }) =>
-        <button className="text-left" key={key}>{key}</button>)
-    }
-  </div>);
+  return (
+    <div className="bg-white m-4 p-4 flex flex-col space-y-2">
+      <div className="font-bold mb-2">{name}</div>
+      {
+        filters.map(({ key }) =>
+          <button className="text-left" key={key}>{key}</button>)
+      }
+    </div>
+  );
 }
