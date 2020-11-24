be2118c9a4993ebe702342f81b8194d1980fcbd5
Chintan Acharya <acharya.chintan96@gmail.com>
2020-11-24 16:47:11 +0530

    Add desktop nav

--- components/nav.js
+++ components/nav.js
@@ -7,24 +7,24 @@ const links = [
 
 export default function Nav() {
   return (
-    <nav>
-      <ul className="flex items-center justify-between p-8">
-        <li>
-          <Link href="/">
-            <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300">
-              Home
-            </a>
-          </Link>
-        </li>
-        <ul className="flex items-center justify-between space-x-4">
-          {links.map(({ href, label }) => (
-            <li key={`${href}${label}`}>
-              <a href={href} className="no-underline btn-blue">
-                {label}
-              </a>
-            </li>
-          ))}
+    <nav className="w-screen bg-white font-semibold text-sm">
+      <ul className="flex items-center justify-between py-2 px-4">
+        <li className="text-blue-500">HEALTH EXPLORE</li>
+        <ul className="flex items-center justify-between space-x-6">
+          <li>PROFILE</li>
+          <li>JOBS</li>
+          <li>PROFESSIONAL NETWORK</li>
+          <li>LOUNGE</li>
+          <li>SALARY</li>
         </ul>
+        <div className="flex space-x-6 items-stretch">
+          <button className="border border-blue-500 text-blue-500 rounded-md font-semibold my-2 px-3">CREATE JOB</button>
+          <div className="relative p-2">
+            <button className="bg-blue-500 h-10 w-10 rounded-full text-white font-semibold">JO</button>
+            <div className="bg-red-500 text-white absolute top-0 right-0 text-xs h-6 w-6 rounded-full border-2 border-white flex justify-center items-center">2</div>
+          </div>
+          <div className="flex items-center">LOGOUT</div>
+        </div>
       </ul>
     </nav>
   )
