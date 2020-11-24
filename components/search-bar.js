2fafb991f24e6bef9e42bbf0e48d432cbec7f310
Chintan Acharya <acharya.chintan96@gmail.com>
2020-11-24 17:52:55 +0530

    Move search bar into its own component

---
+++ components/search-bar.js
@@ -0,0 +1,7 @@
+export default function SearchBar() {
+  return (
+      <div className="m-4 p-4 flex bg-white space-x-4">
+        <input type="text" className="w-full outline-none" placeholder="Search for any job, title, keywords or company" />
+      </div>
+  );
+}
